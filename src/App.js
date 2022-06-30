6515e1f0570e3b05d0fd3abc19a75ad6bdf8d2f7
shameem2001 <toshameem2001@gmail.com>
2022-06-30 23:28:22 +0530

    Connected mongodb database, created file structure, added frontend structure and cdns

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import './app.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './user/pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function Medcare() {
   return (
-    <div>Medcare</div>
-  )
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
