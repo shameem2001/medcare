8e23c72a12655255f8724c0499f2a2bd73d36581
shameem2001 <toshameem2001@gmail.com>
2022-07-07 18:20:31 +0530

    Doctor card added

--- src/user/pages/DoctorList.js
+++ src/user/pages/DoctorList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import HospitalListCard from '../components/HospitalListCard';
+import DoctorListCard from "../components/DoctorListCard";
 
 let hospitals_list = [
   {
@@ -53,12 +54,63 @@ let hospitals_list = [
   },
 ];
 
-export default function Doctor_list() {
+let doctors_list = [
+  {
+    id: 1,
+    img: "profile.png",
+    name: "Feroz BK",
+    department: "Dermatology",
+    experience: 3,
+    hospital: "Sreechand Hospital",
+  },
+  {
+    id: 2,
+    img: "profile.png",
+    name: "Feroz BK",
+    department: "Dermatology",
+    experience: 3,
+    hospital: "Sreechand Hospital",
+  },
+  {
+    id: 3,
+    img: "profile.png",
+    name: "Feroz BK",
+    department: "Dermatology",
+    experience: 3,
+    hospital: "Sreechand Hospital",
+  },
+  {
+    id: 4,
+    img: "profile.png",
+    name: "Feroz BK",
+    department: "Dermatology",
+    experience: 3,
+    hospital: "Sreechand Hospital",
+  },
+  {
+    id: 5,
+    img: "profile.png",
+    name: "Feroz BK",
+    department: "Dermatology",
+    experience: 3,
+    hospital: "Sreechand Hospital",
+  },
+  {
+    id: 6,
+    img: "profile.png",
+    name: "Feroz BK",
+    department: "Dermatology",
+    experience: 3,
+    hospital: "Sreechand Hospital",
+  },
+];
+
+export default function DoctorList() {
   return (
     <div className="parent">
       <div className="container">
         <Link to="/" className="link">
-          <div/>
+          <div />
           <div className="back-to-home">
             <i className="fa-solid fa-angle-left back-icon"></i>
             <h6 className="home-url">Back to Home</h6>
@@ -94,9 +146,7 @@ export default function Doctor_list() {
             />
           </div>
 
-          <button className="btn btn-large search-button">
-            Search
-          </button>
+          <button className="btn btn-large search-button">Search</button>
         </div>
 
         <div className="container shadow doctor-list-card-container">
@@ -124,7 +174,7 @@ export default function Doctor_list() {
             </div>
           </div>
           <div class="container cont3">
-            {hospitals_list.map((hospital) => {
+            {/* {hospitals_list.map((hospital) => {
               return (
                 <HospitalListCard
                   id={hospital.id}
@@ -135,7 +185,21 @@ export default function Doctor_list() {
                   doctors={hospital.doctors}
                 />
               );
+            })} */}
+            <div className="doctor-card-sheet">
+              {doctors_list.map((doctor) => {
+              return (
+                <DoctorListCard
+                  id={doctor.id}
+                  img={doctor.img}
+                  name={doctor.name}
+                  department={doctor.department}
+                  experience={doctor.experience}
+                  hospital={doctor.hospital}
+                />
+              );
             })}
+            </div>
           </div>
         </div>
       </div>
