3eeacaa57ec5869e2c5aa3663afc7733e9d6141e
shameem2001 <toshameem2001@gmail.com>
2022-07-24 21:41:56 +0530

    Profile-page-completed

---
+++ src/user/components/Profile/AppointmentCard.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export default function AppointmentCard() {
+  return (
+    <div className="container shadow-sm profile-tabbar-content-all-tab-appointment-card">
+      <div className="profile-tabbar-content-all-tab-appointment-card-sect-1">
+        <h6>ASTER MIMS, KANNUR</h6>
+        <p>Chala East, Kannur, 670003, Kerala, India, 9446422189</p>
+      </div>
+
+      <div className="profile-tabbar-content-all-tab-appointment-card-sect-2">
+        <div className="profile-tabbar-content-all-tab-appointment-card-sect-2-pic"></div>
+        <div className="profile-tabbar-content-all-tab-appointment-card-sect-2-data">
+          <h6>DR. RADHIKA</h6>
+          <p>DERMATOLOGY</p>
+        </div>
+      </div>
+      <div className="profile-tabbar-content-all-tab-appointment-card-sect-3">
+        <div>
+          <p>Appointment Date</p>
+          <p>Session</p>
+          <p>Time</p>
+          <p>Booking for</p>
+          <p>Appointment Ref Id</p>
+        </div>
+        <div>
+          <p>:</p>
+          <p>:</p>
+          <p>:</p>
+          <p>:</p>
+          <p>:</p>
+        </div>
+        <div className="profile-tabbar-content-all-tab-appointment-card-sect-3-right">
+          <p>03-06-2022</p>
+          <p>AFTERNOON</p>
+          <p>12:00 PM</p>
+          <p>John Doe</p>
+          <p>QK-139646</p>
+        </div>
+      </div>
+      <div className="profile-tabbar-content-all-tab-appointment-card-sect-4">
+        <div className="profile-tabbar-content-all-tab-appointment-card-sect-4-left">
+          <p>Confirmed</p>
+        </div>
+        <div className="profile-tabbar-content-all-tab-appointment-card-sect-4-right">
+          <p>CANCEL</p>
+          <p>VIEW</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/user/pages/ProfilePage/Profile.js
+++ src/user/pages/ProfilePage/Profile.js
@@ -1,5 +1,7 @@
 import React from "react";
+import AppointmentCard from "../../components/Profile/AppointmentCard";
 import DependantCard from "../../components/Profile/DependantCard";
+import PrescriptionCard from "../../components/Profile/PrescriptionCard";
 import './Profile.scss';
 
 export default function Profile() {
@@ -61,7 +63,143 @@ export default function Profile() {
           <button className="btn dependents-card-btn">Add Dependant</button>
         </div>
       </div>
-      <div className="profile-div-right"></div>
+      <div className="profile-div-right">
+        <div className="container shadow profile-tabbar-container">
+          <ul
+            className="nav nav-pills mb-3 profile-tabbar"
+            id="pills-tab"
+            role="tablist"
+          >
+            <li class="nav-item" role="presentation">
+              <button
+                className="nav-link active profile-tabbar-tab"
+                id="pills-home-tab"
+                data-bs-toggle="pill"
+                data-bs-target="#pills-home"
+                type="button"
+                role="tab"
+                aria-controls="pills-home"
+                aria-selected="true"
+              >
+                All
+              </button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link profile-tabbar-tab"
+                id="pills-profile-tab"
+                data-bs-toggle="pill"
+                data-bs-target="#pills-profile"
+                type="button"
+                role="tab"
+                aria-controls="pills-profile"
+                aria-selected="false"
+              >
+                Appointments
+              </button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link profile-tabbar-tab"
+                id="pills-contact-tab"
+                data-bs-toggle="pill"
+                data-bs-target="#pills-contact"
+                type="button"
+                role="tab"
+                aria-controls="pills-contact"
+                aria-selected="false"
+              >
+                History
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div
+            className="tab-content profile-tabbar-contents-container"
+            id="pills-tabContent"
+          >
+            <div
+              className="tab-pane fade show active profile-tabbar-content-all-tab"
+              id="pills-home"
+              role="tabpanel"
+              aria-labelledby="pills-home-tab"
+            >
+              <div className="container shadow profile-tabbar-content-all-tab-overview-container">
+                <h5>Overview</h5>
+                <hr />
+                <ul>
+                  <li>Patient has eczema and asthma.</li>
+                  <li>
+                    Allergic to dust, red meat, cocoa, meat with exoskeletons.
+                  </li>
+                  <li>
+                    Allergic reactions often seen as asthma or itchiness in B/L
+                    orbit and legs.
+                  </li>
+                  <li>No other serious medical conditions.</li>
+                </ul>
+              </div>
+              <div className="container shadow profile-tabbar-content-all-tab-appointments-container">
+                <h5>Latest Appointment</h5>
+                <hr />
+                <AppointmentCard />
+              </div>
+              <div className="container shadow profile-tabbar-content-all-tab-history-container">
+                <h5>History</h5>
+                <hr />
+                <div
+                  id="accordion"
+                  className="profile-tabbar-content-all-tab-history-accordion"
+                >
+                  <PrescriptionCard no={"1"} />
+                  <PrescriptionCard no={"2"} />
+                  <PrescriptionCard no={"3"} />
+                </div>
+              </div>
+            </div>
+            <div
+              className="tab-pane fade"
+              id="pills-profile"
+              role="tabpanel"
+              aria-labelledby="pills-profile-tab"
+            >
+              <div className="container shadow profile-tabbar-content-all-tab-appointments-container">
+                <h5 className="profile-tabbar-content-appointments-tab-container-h5">
+                  Appointments
+                </h5>
+                <hr />
+                <div className="profile-tabbar-content-appointments-tab-container">
+                  <AppointmentCard />
+                  <AppointmentCard />
+                  <AppointmentCard />
+                  <AppointmentCard />
+                </div>
+              </div>
+            </div>
+            <div
+              className="tab-pane fade"
+              id="pills-contact"
+              role="tabpanel"
+              aria-labelledby="pills-contact-tab"
+            >
+              <div className="container shadow profile-tabbar-content-all-tab-history-container">
+                <h5>History</h5>
+                <hr />
+                <div
+                  id="accordion"
+                  className="profile-tabbar-content-all-tab-history-accordion"
+                >
+                  <PrescriptionCard no={"1"} />
+                  <PrescriptionCard no={"2"} />
+                  <PrescriptionCard no={"3"} />
+                  <PrescriptionCard no={"4"} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
