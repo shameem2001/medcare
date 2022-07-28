0edcdc925425a113cd7f24cc79f531c0c9bac700
Aiswarya K <anandkenoth47@gmail.com>
2022-07-28 16:29:13 +0530

    Home_page_updated

--- src/user/pages/HomePage/Home.js
+++ src/user/pages/HomePage/Home.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import './Home.scss'
+
+// import About from '../../components/About'
+import Banner from '../../components/HomePage/Banner'
+import Contact from '../../components/HomePage/Contact'
+import Footer from '../../components/HomePage/Footer'
+import Header from '../../components/HomePage/Header'
+import Service from '../../components/HomePage/Service'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+
+        <Header />
+        <Banner />
+        <Service />
+        {/* <About /> */}
+        <Contact />
+        <Footer />
+    </>
   )
 }
